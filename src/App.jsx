364ae960cc5d108a6192470d886f3745e16b8708
Shrijakandpal <shrija.kandpal30@gmail.com>
2025-08-13 15:51:21 +0530

    fix navbar case for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Contact } from './components/Contact/Contact';
 import { Experience } from './components/Experience/Experience';
 
 import { Hero } from './components/Hero/Hero';
-import Navbar from './components/NavBar/Navbar';
+import {Navbar} from './components/Navbar/Navbar';
 import { Projects } from './components/Projects/Projects';
 function App(){
   return (
